4dea42a725bc26f33c1b9819d1bb772ce809215f
purbon <pere.urbon@gmail.com>
2013-02-08 12:11:35 +0100

    add main and tmp graphs, within the hole lib

--- db/src/main/java/com/purbon/db/GNetty.java
+++ db/src/main/java/com/purbon/db/GNetty.java
@@ -1,10 +1,32 @@
 package com.purbon.db;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.purbon.db.Impl.GraphImpl;
 
 public class GNetty {
 
+	private Map<String, Graph> tmpGraphs;
+	private Graph graph;
+	
+	public GNetty() {
+		tmpGraphs = new HashMap<String, Graph>();
+		graph     = new GraphImpl("main");
+	}
+	
 	public Graph newGraph(String name) {
-		return new GraphImpl(name);
+		Graph graph = new GraphImpl(name);
+		tmpGraphs.put(name, graph);
+		return graph;
+	}
+	
+
+	public Graph getTempGraph(String name) {
+		return tmpGraphs.get(name);
+	}
+	
+	public Graph getGraph() {
+		return graph;
 	}
 }
